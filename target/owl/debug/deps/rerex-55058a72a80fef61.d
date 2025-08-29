0d67a966e500e40c35fb2976f95e2cc765d98a71
marcel <marcel.hajek@gmx.de>
2025-08-29 09:28:56 +0200

    added .gitignore and removed /target dir

--- target/owl/debug/deps/rerex-55058a72a80fef61.d
+++
@@ -1,9 +0,0 @@
-/home/marcel/code/rarex/target/owl/debug/deps/librerex-55058a72a80fef61.rmeta: src/main.rs src/lexer/mod.rs src/tokens/mod.rs src/tokens/keywords.rs src/tokens/ops.rs
-
-/home/marcel/code/rarex/target/owl/debug/deps/rerex-55058a72a80fef61.d: src/main.rs src/lexer/mod.rs src/tokens/mod.rs src/tokens/keywords.rs src/tokens/ops.rs
-
-src/main.rs:
-src/lexer/mod.rs:
-src/tokens/mod.rs:
-src/tokens/keywords.rs:
-src/tokens/ops.rs:
